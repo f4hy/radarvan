c417ebe8cc7c464fc83f038c54fad50c6a41e110
Brendan Fahy <bmfahy@gmail.com>
2025-10-12 11:25:48 -0400

    tweak config

--- src/Client.ts
+++ src/Client.ts
@@ -1,7 +1,15 @@
 import { DefaultApi, Configuration, DefaultConfig } from "./api"
 
-const config = new Configuration({
+function getConfig(): Configuration {
+  // Using a framework-agnostic check for NODE_ENV
+  if (process.env.NODE_ENV === 'development') {
+    return new Configuration({
   basePath: 'http://localhost:8000',
 });
+  }
+  // This will be used in production
+  return DefaultConfig;
+}
+const config = getConfig()
 export const Client = new DefaultApi(config)
 
