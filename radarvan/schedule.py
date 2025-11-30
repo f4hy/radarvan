a8a4b147fa6a637ce112cc4feba62871355754af
Brendan Fahy <bmfahy@gmail.com>
2025-11-30 10:16:41 -0500

    notify debug of update

--- radarvan/schedule.py
+++ radarvan/schedule.py
@@ -3,17 +3,23 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime
 import scrape_games
 import logging
+from notify import notify
 
 logger = logging.getLogger(__name__)
 
 
-async def update_games(replay_manager: ReplayManager, days: int = 0) -> None:
+async def update_games(
+    replay_manager: ReplayManager,
+    days: int = 0,
+    notify: bool = False,
+) -> None:
     """Get latest updates."""
     logger.info("Updating games.")
     base = scrape_games.BASE
     paths = await scrape_games.get_replay_urls(days, base, replay_manager)
     msg = f"Done updating, found {len(paths)}."
     logger.info(msg)
+    notify("DEBUG:" + msg)
 
 
 def get_scheduler(replay_manager: ReplayManager) -> AsyncIOScheduler:
@@ -31,7 +37,7 @@ def get_scheduler(replay_manager: ReplayManager) -> AsyncIOScheduler:
         update_games,
         "interval",
         minutes=60,
-        args=[replay_manager],
+        args=[replay_manager, 1, True],
         id="update_games",
     )
     logger.info("Setup scheduler.")
