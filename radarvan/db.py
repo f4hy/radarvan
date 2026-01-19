ac5c3f253370a544894bd4ac6d747ce239613212
Brendan Fahy <bmfahy@gmail.com>
2026-01-18 20:32:49 -0500

    fix?

--- radarvan/db.py
+++ radarvan/db.py
@@ -78,7 +78,7 @@ class ParsedReplayJson(Base):
     )
 
     # File info
-    num_time_stamps = Column(Integer, nullable=False, default=0)
+    num_time_stamps = Column(Integer, default=0)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     game_timestamp = Column(DateTime, nullable=False)
     game_date = Column(Date, index=True)
