2b236d76433dfbdf8030da603dd3f8a0be258b31
Brendan Fahy <bmfahy@gmail.com>
2026-03-14 15:34:56 -0400

    fix

--- alembic/versions/a1b2c3d4e5f6_add_map_data.py
+++ alembic/versions/a1b2c3d4e5f6_add_map_data.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, Sequence[str], None] = '8da58256e2e7'
+down_revision: Union[str, Sequence[str], None] = '32bc714f0ca1'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
