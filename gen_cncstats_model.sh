c8ad70efd0efc4c2e9e42be2feedd98d4b5a3b7e
Brendan Fahy <bmfahy@gmail.com>
2026-03-19 02:22:39 -0400

    add new json format

--- gen_cncstats_model.sh
+++ gen_cncstats_model.sh
@@ -4,4 +4,16 @@ set -e
 
 curl -o cncstats_schema.json http://cncstats.computersrfun.org:8080/swagger/doc.json
 
-datamodel-codegen  --input cncstats_schema.json --input-file-type jsonschema --output radarvan/cncstats_model/
+datamodel-codegen  \
+		--input cncstats_schema.json\
+		--output radarvan/cncstats_model/ \
+		--input-file-type jsonschema \
+		--output-model-type pydantic_v2.BaseModel \
+		--use-annotated \
+		--use-standard-collections \
+		--use-union-operator \
+		--field-constraints \
+		--snake-case-field \
+		--collapse-root-models \
+		--disable-timestamp
+
