f0a094aabe594b009b2b8676ba2a12bb1218a9cf
Brendan Fahy <bmfahy@gmail.com>
2026-07-04 01:06:22 -0400

    typo

--- gen_client.sh
+++ gen_client.sh
@@ -2,4 +2,3 @@
 
 # npx @hey-api/openapi-ts -i http://localhost:5000/openapi.json -o src/generated_client -p @hey-api/client-fetch
 openapi-generator-cli generate -i http://localhost:8000/openapi.json -g typescript-fetch -o src/api --additional-properties=typescriptThreePlus=true
-g
